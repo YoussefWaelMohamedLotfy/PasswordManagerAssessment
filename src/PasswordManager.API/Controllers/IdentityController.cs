5a887b14990cc39ad9fd09cee2896994dc323824
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-07-22 16:39:49 +0200

    Created System's API Project with JWT Bearer Auth Support

---
+++ src/PasswordManager.API/Controllers/IdentityController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PasswordManager.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() 
+        => new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+}
