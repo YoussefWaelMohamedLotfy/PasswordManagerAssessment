e0c0f3257d6a4bfad74891d6675974fd496bdcc2
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-07-26 16:51:34 +0200

    Added Domain Model for Social Credential

---
+++ src/PasswordManager.API/Models/SocialCredential.cs
@@ -0,0 +1,16 @@
+﻿namespace PasswordManager.API.Models;
+
+public class SocialCredential
+{
+    public int ID { get; set; }
+
+    public string SubjectID { get; set; } = default!;
+    
+    public CredentialAppName Name { get; set; } = default!;
+    
+    public string AccountUsername { get; set; } = default!;
+    
+    public string AccountPassword { get; set; } = default!;
+
+    public bool IsActive { get; set; }
+}
