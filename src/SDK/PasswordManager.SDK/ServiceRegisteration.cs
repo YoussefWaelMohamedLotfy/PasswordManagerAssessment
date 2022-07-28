1517045dc6a8e6c3ee3b9325e69bc2985492a642
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-07-28 12:59:59 +0200

    Added SDK project

---
+++ src/SDK/PasswordManager.SDK/ServiceRegisteration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PasswordManager.SDK;
+
+public static class ServiceRegisteration
+{
+    public static void AddRefitHttpClients(this IServiceCollection services)
+    {
+
+    }
+}
