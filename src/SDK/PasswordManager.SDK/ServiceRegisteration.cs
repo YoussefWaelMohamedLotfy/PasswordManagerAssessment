f138351f5c392c23260db44b757bfecdc959b323
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2022-07-28 14:18:08 +0200

    Added Retry & Circuit Breaker Patterns to Http Clients

--- src/SDK/PasswordManager.SDK/ServiceRegisteration.cs
+++ src/SDK/PasswordManager.SDK/ServiceRegisteration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using PasswordManager.Logging;
+using Polly.Extensions.Http;
+using Polly;
 using Refit;
+using Serilog;
 
 namespace PasswordManager.SDK;
 
@@ -10,6 +13,40 @@ public static class ServiceRegisteration
     {
         services.AddRefitClient<IPasswordManagerApi>()
             .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7001"))
-            .AddHttpMessageHandler<LoggingDelegatingHandler>();
+            .AddHttpMessageHandler<LoggingDelegatingHandler>()
+             .AddPolicyHandler(GetRetryPolicy())
+             .AddPolicyHandler(GetCircuitBreakerPolicy());
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    {
+        /* 
+         * In this case will wait for
+         * 2 ^ 1 = 2 seconds then
+         * 2 ^ 2 = 4 seconds then
+         * 2 ^ 3 = 8 seconds then
+         * 2 ^ 4 = 16 seconds then
+         * 2 ^ 5 = 32 seconds
+        */
+
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .WaitAndRetryAsync(
+                retryCount: 5,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (exception, retryCount, context) =>
+                {
+                    Log.Error($"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                });
+    }
+
+    private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .CircuitBreakerAsync(
+                handledEventsAllowedBeforeBreaking: 5,
+                durationOfBreak: TimeSpan.FromSeconds(30)
+            );
     }
 }
